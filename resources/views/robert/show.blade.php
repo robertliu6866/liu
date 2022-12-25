ee44c1751d77611b6bcf33d60f0a410ae52b32a4
劉展宏 <>
2022-12-25 09:36:23 +0800

    no message

--- resources/views/robert/show.blade.php
+++ resources/views/robert/show.blade.php
@@ -50,7 +50,7 @@
              <img class="inline-block h-6 w-6 rounded-full ring-2 ring-white" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
            </div>
         
-          <p>{{$robert2->excerpt}}</p>
+          {{-- <p>{{$robert2->excerpt}}</p> --}}
              
             </div>
           
